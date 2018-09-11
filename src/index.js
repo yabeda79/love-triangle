e72430ee8e4655f3f063ad00eca6125ea52510bd
yabeda79 <43182987+yabeda79@users.noreply.github.com>
2018-09-11 22:09:52 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(a) {
+let count=0, i=0
+for(i=0; i<a.length; i++){
+  if(a[a[a[i]-1]-1]==i+1) {
+    count++
+  }
+}
+return Math.floor(count/3)
 };
